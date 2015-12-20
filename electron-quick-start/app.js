f07f73a7c7539eb1c2fefc48ddd05968ef7376ad
Tyrubias <victor.song.769@k12.friscoisd.org>
2015-12-20 15:35:47 -0600

    removed auto-generated merge conflict message

--- electron-quick-start/app.js
+++ electron-quick-start/app.js
@@ -5,10 +5,7 @@ var child;
 child = exec("java -v", function (error, stdout, stderr) {
   console.log('stdout: ' + stdout);
   console.log('stderr: ' + stderr);
-<<<<<<< HEAD
-=======
   output = stderr;
->>>>>>> origin/master
   document.getElementById("test").innerHTML = output;
   if (error !== null) {
     console.log('exec error: ' + error);
