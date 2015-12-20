93eb23f97f465f50f0b4b9856bb05ae02cb290c7
geooot <george.thayamkery@gmail.com>
2015-12-20 11:53:04 -0600

    a

--- electron-quick-start/app.js
+++ electron-quick-start/app.js
@@ -2,10 +2,10 @@ var exec = require('child_process').exec;
 var output = '';
 var child;
 // executes `pwd`
-child = exec("npm -version", function (error, stdout, stderr) {
+child = exec("java -v", function (error, stdout, stderr) {
   console.log('stdout: ' + stdout);
   console.log('stderr: ' + stderr);
-  output = stdout;
+  output = stderr;
   document.getElementById("test").innerHTML = output;
   if (error !== null) {
     console.log('exec error: ' + error);
